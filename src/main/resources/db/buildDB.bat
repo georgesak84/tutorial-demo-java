809bbcb1b33a87be096dfe34a96a9dc38fa7c9d7
Georges Abou Khalil <georges.aboukhalil@hotmail.com>
2018-08-20 12:21:43 +0300

    adding the source files

---
+++ src/main/resources/db/buildDB.bat
@@ -0,0 +1,9 @@
+@echo off
+SET DIR=%~d0%~p0%
+
+SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
+SET repository.path="https://github.com/espiderinc/tutorial-demo-java.git"
+SET version.file="..\..\..\..\pom.xml"
+SET version.xpath="//project/version"
+
+"C:\Users\georges_aboukhalil\Downloads\RoundhousE.v0.8.6.0\RoundhousE\console\rh.exe" /vf=%version.file% /vx=%version.xpath% /c=%connstring% /r=%repository.path% /env=LOCAL /ni /simple
